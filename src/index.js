014b53ffeadc9d72a2052539a015180c7b96ebbd
Adam Weesner <Adam.Weesner@target.com>
2019-09-21 13:03:37 -0500

    updated start page and added theme back

--- src/index.js
+++ src/index.js
@@ -2,11 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#5fa463',
+            main: '#388e3c',
+            dark: '#27632a',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#f73378',
+            main: '#f50057',
+            dark: '#ab003c',
+            contrastText: '#fff',
+        },
+    },
+    typography: {
+        useNextVariants: true,
+      },
+})
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
