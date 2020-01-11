463c46abd72ced469d39d6d8de650497e6bd4c9b
Adam Weesner <alweesner@gmail.com>
2020-01-10 18:38:30 -0600

    Updates

---
+++ precache-manifest.7bf6a14cdd7b1464b778e1f80e6b5153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fc4fbc34fb024343e0ff5cda741300",
+    "url": "/WeesnerDevelopment-react/index.html"
+  },
+  {
+    "revision": "4f43868ea2671d68b0b2",
+    "url": "/WeesnerDevelopment-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fcf300d576e470bd985d",
+    "url": "/WeesnerDevelopment-react/static/js/2.949cbd60.chunk.js"
+  },
+  {
+    "revision": "ef73c2409dfc5f2f3f6aa059bf37a96d",
+    "url": "/WeesnerDevelopment-react/static/js/2.949cbd60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f43868ea2671d68b0b2",
+    "url": "/WeesnerDevelopment-react/static/js/main.e7d46432.chunk.js"
+  },
+  {
+    "revision": "8c60abcb6bed4a7e3d57",
+    "url": "/WeesnerDevelopment-react/static/js/runtime-main.19c96ce6.js"
+  }
+]);
